38e3aaa2e0ba531cef9fb5dfcd73b9d67468913d
mamipeko <mamipeko@mamipekobook2020.local>
2020-04-27 11:53:40 +0900

    :memo: テキストの後ろに点滅カーソルをつける

--- docs/common/js/common.js
+++ docs/common/js/common.js
@@ -1,28 +1,28 @@
 /* cover
 ================================================== */
 function cover() {
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 cover();
 
 /* preload
 ================================================== */
 function preload() {
-    for(var i = 0; i< arguments.length; i++){
-        $("<img>").attr("src", arguments[i]);
-    }
+	for (var i = 0; i < arguments.length; i++) {
+		$("<img>").attr("src", arguments[i]);
+	}
 }
 preload('/common/img/logo/logo-01.png',
-		'/common/img/logo/logo-02.png',
-		'/common/img/logo/logo-03.png',
-		'/common/img/logo/logo-04.png',
-		'/common/img/logo/logo-05.png',
-		'/common/img/logo/logo-06.png',
-		'/common/img/logo/logo-07.png',
-		'/common/img/logo/logo-08.png',
-		'/common/img/logo/logo-09.png',
-		'/common/img/logo/logo.png');
+	'/common/img/logo/logo-02.png',
+	'/common/img/logo/logo-03.png',
+	'/common/img/logo/logo-04.png',
+	'/common/img/logo/logo-05.png',
+	'/common/img/logo/logo-06.png',
+	'/common/img/logo/logo-07.png',
+	'/common/img/logo/logo-08.png',
+	'/common/img/logo/logo-09.png',
+	'/common/img/logo/logo.png');
 
 /*
 Licensed under the MIT license.
@@ -32,5 +32,6 @@ https://codepen.io/michaelnarine/pen/RKpBLg
 ================================================== */
 var pix = document.getElementsByClassName("pixel");
 for (var i = 0; i < pix.length; i++) {
-  pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
+	pix[i].style.animationDelay = Math.ceil(Math.random() * 5000) + "ms";
 }
+
