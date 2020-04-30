23ddd3941dfe1d4a693f627529180691b5e7fb30
mamipeko <mamipeko@mamipekobook2020.local>
2020-04-30 11:56:21 +0900

    :art: カーソルを指定する

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -1,7 +1,7 @@
 /*
 Licensed under the MIT license.
 http://opensource.org/licenses/mit-license.php
-Copyright © 2020 Michael
+Copyright (c) 2020 Michael
 https://codepen.io/michaelnarine/pen/RKpBLg
 ================================================== */
 var pix = document.getElementsByClassName("pixel");
@@ -18,7 +18,7 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 /*
 Licensed under the MIT license.
 http://opensource.org/licenses/mit-license.php
-Copyright © 2015 Michał Sajnóg
+Copyright (c) 2015 Michał Sajnóg
 https://github.com/michalsnik/aos
 ================================================== */
 AOS.init({
@@ -29,3 +29,23 @@ AOS.init({
 	mirror: false,
 	anchorPlacement: 'top-bottom',
 });
+
+/* cursor
+Licensed under the MIT license.
+http://opensource.org/licenses/mit-license.php
+Copyright (c) 2020 Murtaza
+https://codepen.io/murtazamzk/pen/zYGBeva
+================================================== */
+const root = document.querySelector('html');
+const cursor = document.createElement('span');
+cursor.classList.add('cursor');
+root.appendChild(cursor);
+const cursorInner = document.createElement('span');
+cursorInner.classList.add('cursor', 'cursor__inner');
+cursor.appendChild(cursorInner);
+root.addEventListener('mousemove', e => {
+	setPosition(cursor, e);
+});
+function setPosition(element, e) {
+	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+}
