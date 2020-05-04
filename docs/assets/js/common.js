63ab94ab3265464a52b658765eccb158d5771371
mamipeko <mamipeko@mamipekobook2020.local>
2020-05-04 15:29:14 +0900

    :muscle: jQuery の書き方で揃える

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -1,58 +1,39 @@
-/*
-Copyright (c) 2020 by Michael (https://codepen.io/michaelnarine/pen/RKpBLg)
-================================================== */
-var pix = document.getElementsByClassName("pixel");
-for (var i = 0; i < pix.length; i++) {
-	pix[i].style.animationDelay = Math.ceil(Math.random() * 5000) + "ms";
-}
-$(".pixel").addClass("pixel-animation");
+$(".pixel").each(function () {
+	$(this)
+		.css("animationDelay", Math.ceil(Math.random() * 5000) + "ms")
+		.addClass("pixel-animation");
+});
 
-/* Full Screen
-================================================== */
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+$("html").css("--vh", (window.innerHeight * 0.01) + "px");
 
-/*
-Copyright (c) 2015 by Michał Sajnóg (https://github.com/michalsnik/aos)
-================================================== */
 AOS.init({
 	offset: 100,
 	delay: 100,
 	duration: 900,
 	once: false,
 	mirror: false,
-	anchorPlacement: 'top-bottom',
+	anchorPlacement: "top-bottom",
 });
 
-/* cursor
-Copyright (c) 2020 by Murtaza (https://codepen.io/murtazamzk/pen/zYGBeva)
-================================================== */
-const root = document.querySelector('html');
-const cursor = document.createElement('span');
-cursor.classList.add('cursor');
-root.appendChild(cursor);
-const cursorInner = document.createElement('span');
-cursorInner.classList.add('cursor', 'cursor__inner');
-cursor.appendChild(cursorInner);
-root.addEventListener('mousemove', e => {
-	setPosition(cursor, e);
-});
-function setPosition(element, e) {
-	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+function setCursorPosition(event) {
+	$(".cursor").css("transform", `translate3d(${event.clientX}px, ${event.clientY}px, 0)`);
 }
 
-/* 
-Copyright (c) 2020 by ryohei (https://codepen.io/intotheprogram/pen/GPRXEP)
-================================================== */
-$(window).on('load scroll', function () {
-	var elem = $('.mask');
-	elem.each(function () {
-		var isPlay = 'isPlay';
-		var elemOffset = $(this).offset().top;
-		var scrollPos = $(window).scrollTop();
+$("<span/>")
+	.addClass("cursor")
+	.append($("<span/>").addClass("cursor").addClass("cursor-inner"))
+	.appendTo($("body"));
+
+$("body")
+	.on("mousemove", function (event) {
+		setCursorPosition(event);
+	});
+
+$(window).on("scroll", function () {
+	$('.mask').each(function () {
 		var wh = $(window).height();
-		if (scrollPos > elemOffset - wh + (wh / 4)) {
-			$(this).addClass(isPlay);
+		if ($(window).scrollTop() > $(this).offset().top - wh + (wh / 4)) {
+			$(this).addClass("is-play");
 		}
 	});
 });
