7d615b66b02f9c6962526ebcf61a8a362b21e16d
mamipeko <mamipekonet@gmail.com>
2020-05-05 18:50:42 +0900

    :sparkles: 処理を共通化する

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -1,7 +1,6 @@
 $(".pixel").each(function () {
 	$(this).css("animationDelay", Math.ceil(Math.random() * 5000) + "ms");
 });
-$(".pixel-list").addClass("pixel-animation");
 
 var windowHeight;
 
@@ -24,53 +23,13 @@ $(window).on("load scroll", _.throttle(function () {
 			$(this).removeClass("animationable-active");
 		}
 	});
-
-	/* pixel
-	================================================== */
-	$(".photo").each(function () {
-		if ($(this).offset().top < $(window).innerHeight() + $(window).scrollTop() &&
-			$(this).offset().top + $(this).height() > $(window).scrollTop()) {
-			$(this).addClass("pixel-active");
-		}
-		else {
-			$(this).removeClass("pixel-active");
-		}
-	});
-
-	$(".title").each(function () {
-		if ($(this).offset().top < $(window).innerHeight() + $(window).scrollTop() &&
-			$(this).offset().top + $(this).height() > $(window).scrollTop()) {
-			$(this).addClass("title-active");
-		}
-		else {
-			$(this).removeClass("title-active");
-		}
-	});
-
-	$(".button").each(function () {
-		if ($(this).offset().top < $(window).innerHeight() + $(window).scrollTop() &&
-			$(this).offset().top + $(this).height() > $(window).scrollTop()) {
-			$(this).addClass("button-active");
-		}
-		else {
-			$(this).removeClass("button-active");
-		}
-	});
-
-	$(".summary").each(function () {
-		if ($(this).offset().top < $(window).innerHeight() + $(window).scrollTop() &&
-			$(this).offset().top + $(this).height() > $(window).scrollTop()) {
-			$(this).addClass("summary-active");
-		}
-		else {
-			$(this).removeClass("summary-active");
-		}
-	});
 }, 400));
 
-function setCursorPosition(event) {
-	$(".cursor-wrapper").css("transform", `translate3d(${event.clientX}px, ${event.clientY}px, 0)`);
-}
+$(window).on("load scroll", _.throttle(function () {
+	if ($(window).scrollTop() > $(window).innerHeight()) {
+		$(".cursor").addClass("cursor-active");
+	}
+}, 20));
 
 $("<span/>")
 	.addClass("cursor-wrapper")
@@ -79,14 +38,8 @@ $("<span/>")
 
 $("body")
 	.on("mousemove", _.throttle(function (event) {
-		setCursorPosition(event);
+		$(".cursor-wrapper").css("transform", `translate3d(${event.clientX}px, ${event.clientY}px, 0)`);
 	}, 50));
-
-$(window).on("load scroll", _.throttle(function () {
-	if ($(window).scrollTop() > $(window).innerHeight()) {
-		$(".cursor").addClass("cursor-active");
-	}
-}, 20));
 
 $("html").css("--vh", (window.innerHeight * 0.01) + "px");
 
